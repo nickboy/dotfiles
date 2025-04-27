a550c31af696973546222318558e0fedf1a7e649
Tzu-Hua(Nick) Liu <nickboy@users.noreply.github.com>
2025-04-26 20:45:43 -0700

    Yadm (#1)
    * Remove all file and add .zshrc with yadm
    
    * Add Brewfile
    
    * Add .gitconfig
    
    * Add config files
    
    * Update NeoVim and Brewfile
    
    * Add .tmux.conf
    
    ---------
    
    Co-authored-by: Nick Liu <nickboy@NickdeMac-mini.local>

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,25 @@
+vim.g.mapleader = " "
+
+vim.scriptendcoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 0
+vim.opt.laststatus = 0
+vim.opt.expandtab = true
+vim.opt.scrolloff = 100
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+
+vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_ruff = "ruff"
